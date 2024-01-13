f855c9853d9424ad0a6d9476aaf6ab940aa63b17
x2w <x2w.sloth@gmail.com>
2024-01-13 16:05:42 +0800

    Header, add debug canary macro

--- Ludens/Core/Header/Include/Error.h
+++ Ludens/Core/Header/Include/Error.h
@@ -6,7 +6,15 @@
 // - mostly used to assert prerequisites and crash early in debug builds
 #ifndef LD_DEBUG_ASSERT
 # include <cassert>
-# define LD_DEBUG_ASSERT(EXPR)       assert(EXPR)
+# define LD_DEBUG_ASSERT(EXPR)             assert(EXPR)
+#endif
+
+
+// Debug Canaries
+// - the asserted expression and canary will be stripped in release builds
+// - the canary is anything that is compliant with the function call syntax
+#ifndef LD_DEBUG_CANARY
+# define LD_DEBUG_CANARY(EXPR, CANARY)     if (!static_cast<bool>(EXPR)) { CANARY( #EXPR ); }
 #endif
 
 
@@ -14,5 +22,5 @@
 // - the asserted expression is checked by the compiler at compile time
 // - does not need to be in function body
 #ifndef LD_STATIC_ASSERT
-# define LD_STATIC_ASSERT(EXPR)      static_assert(EXPR, "")
+# define LD_STATIC_ASSERT(EXPR)            static_assert(EXPR, "")
 #endif
