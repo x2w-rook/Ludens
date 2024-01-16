a086e94218e77b40795e39dd61f50484f88c0508
x2w <x2w.sloth@gmail.com>
2024-01-16 11:35:16 +0800

    OS, heap memory allocation
    fix typo in smart pointers, how did MSVC manage to compile the typo
    without errors for the past week?

--- Ludens/Core/OS/Include/Memory.h
+++ Ludens/Core/OS/Include/Memory.h
@@ -3,15 +3,26 @@
 #include <memory>
 
 namespace LD {
-
 	
+	///
+	/// HEAP MEMORY
+	///
+	
+	void* MemoryAlloc(size_t size);
+	void* MemoryRealloc(void* mem, size_t size);
+	void MemoryFree(void* mem);
+
+	///
+	/// SMART POINTERS
+	///
+
 	template<typename T>
 	using Own = std::unique_ptr<T>;
 
 	template<typename T, typename... TArgs>
 	Own<T> MakeOwn(TArgs&&... args)
 	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
+		return std::make_unique<T>(std::forward<TArgs>(args)...);
 	}
 
 	template<typename T>
@@ -20,8 +31,7 @@ namespace LD {
 	template<typename T, typename... TArgs>
 	Ref<T> MakeRef(TArgs&&... args)
 	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
+		return std::make_shared<T>(std::forward<TArgs>(args)...);
 	}
-
 
 } // namespace LD
