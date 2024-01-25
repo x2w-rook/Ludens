fa06348f41ad8bb81b24376a623b8804a76d9256
x2w <x2w.sloth@gmail.com>
2024-01-25 17:38:09 +0800

    Math, bit twiddling
    forgot Math module CMakeLists.txt

--- Ludens/Core/Math/Tests/MathTests.cpp
+++ Ludens/Core/Math/Tests/MathTests.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include "Core/Math/Tests/TestBits.h"
 #include "Core/Math/Tests/TestVec2.h"
 #include "Core/Math/Tests/TestVec3.h"
 #include "Core/Math/Tests/TestVec4.h"
