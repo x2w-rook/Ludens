5cd1b8146b46921e9f777848aa45d6e00c234308
x2w <x2w.sloth@gmail.com>
2024-04-25 19:45:56 +0800

    DSA, Buffer on the heap

--- Ludens/Core/DSA/Tests/DSATests.cpp
+++ Ludens/Core/DSA/Tests/DSATests.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include "Core/DSA/Tests/TestBuffer.h"
 #include "Core/DSA/Tests/TestArray.h"
 #include "Core/DSA/Tests/TestVector.h"
 #include "Core/DSA/Tests/TestOptional.h"
