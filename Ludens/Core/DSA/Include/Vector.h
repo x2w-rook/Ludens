dbf0ceaa431e6c5871afa1aca1f636f7bd28fa20
x2w <x2w.sloth@gmail.com>
2024-04-26 21:37:43 +0800

    DSA, Vector refactor based on Buffer
    and clang-format

--- Ludens/Core/DSA/Include/Vector.h
+++ Ludens/Core/DSA/Include/Vector.h
@@ -1,167 +1,226 @@
 #pragma once
 
+#include <cstring>
 #include <initializer_list>
 #include "Core/Header/Include/Error.h"
 #include "Core/Math/Include/Bits.h"
 #include "Core/DSA/Include/View.h"
+#include "Core/DSA/Include/Buffer.h"
 #include "Core/OS/Include/Memory.h"
 #include "Core/OS/Include/Allocator.h"
 
 namespace LD {
 
-	template <typename T, typename TAllocator = MemoryAllocator>
-	class Vector
-	{
-	public:
-		Vector()
-			: mSize(0)
-			, mData(nullptr)
-			, mAllocSize(0)
-		{
-		}
+template <typename T>
+class VectorBase
+{
+public:
+    VectorBase() : mData(nullptr), mSize(0)
+    {
+    }
 
-		Vector(size_t size)
-		{
-			Resize(size);
-		}
+    // derived class implements Resize such that
+    // mSize is set to parameter size
+    // mData can be indexed by indices in the range [0, size)
+    virtual void Resize(size_t size) = 0;
 
-		Vector(const Vector<T>& other)
-		{
-			Resize(other.mSize);
-			std::copy(other.Begin(), other.End(), Begin());
-		}
+    inline T* Data()
+    {
+        return mData;
+    }
 
-		Vector(const std::initializer_list<T>& list)
-		{
-			Resize(list.size());
-			std::copy(list.begin(), list.end(), Begin());
-		}
+    inline const T* Data() const
+    {
+        return mData;
+    }
 
-		~Vector()
-		{
-			Deallocate();
-		}
+    inline bool IsEmpty() const
+    {
+        return mSize == 0;
+    }
 
-		Vector<T>& operator=(const Vector<T>& other)
-		{
-			Resize(other.mSize);
-			std::copy(other.Begin(), other.End(), Begin());
-			return *this;
-		}
+    inline size_t Size() const
+    {
+        return mSize;
+    }
 
-		inline T* Data() { return mData; }
-		inline const T* Data() const { return mData; }
-		inline bool IsEmpty() const { return mSize == 0; }
-		inline size_t Size() const { return mSize; }
-		inline size_t ByteSize() const { return sizeof(T) * mSize; }
+    inline size_t ByteSize() const
+    {
+        return sizeof(T) * mSize;
+    }
 
-		inline void Resize(size_t size)
-		{
-			size_t newAllocSize = NextPowerOf2((u32)size);
+    inline void PushBack(const T& item)
+    {
+        Resize(mSize + 1);
+        mData[mSize - 1] = item;
+    }
 
-			// shrinking, or growing but does not trigger realloc
-			if (size <= mSize || newAllocSize <= mAllocSize)
-			{
-				mSize = size;
-				return;
-			}
+    inline void PushBack(T&& item)
+    {
+        Resize(mSize + 1);
+        mData[mSize - 1] = std::move(item);
+    }
 
-			// reallocate, migrate data
-			T* newData = (T*)mAllocator.Alloc(sizeof(T) * newAllocSize);
-			for (size_t i = 0; i < newAllocSize; i++)
-				new (newData + i) T{};
+    inline void PopBack()
+    {
+        if (mSize == 0)
+            return;
 
-			if (mData != nullptr)
-			{
-				for (size_t i = 0; i < mAllocSize; i++)
-				{
-					if (i < mSize)
-						newData[i] = std::move(mData[i]);
-					mData[i].~T();
-				}
-				mAllocator.Free(mData);
-			}
+        Resize(mSize - 1);
+    }
 
-			mAllocSize = newAllocSize;
-			mData = newData;
-			mSize = size;
-		}
+    inline void Clear()
+    {
+        Resize(0);
+    }
 
-		inline void PushBack(const T& item)
-		{
-			Resize(mSize + 1);
-			mData[mSize - 1] = item;
-		}
+    inline T& Front()
+    {
+        LD_DEBUG_ASSERT(mSize > 0);
+        return mData[0];
+    }
 
-		inline void PushBack(T&& item)
-		{
-			Resize(mSize + 1);
-			mData[mSize - 1] = std::move(item);
-		}
+    inline const T& Front() const
+    {
+        LD_DEBUG_ASSERT(mSize > 0);
+        return mData[0];
+    }
 
-		inline void PopBack()
-		{
-			if (mSize == 0)
-				return;
+    inline T& Back()
+    {
+        LD_DEBUG_ASSERT(mSize > 0);
+        return mData[mSize - 1];
+    }
 
-			Resize(mSize - 1);
-		}
+    inline const T& Back() const
+    {
+        LD_DEBUG_ASSERT(mSize > 0);
+        return mData[mSize - 1];
+    }
 
-		inline void Clear() { mSize = 0; }
+    inline const T* Begin() const
+    {
+        return mData;
+    }
 
-		inline T&       Front()       { LD_DEBUG_ASSERT(mSize > 0); return mData[0]; }
-		inline const T& Front() const { LD_DEBUG_ASSERT(mSize > 0); return mData[0]; }
-		inline T&       Back()        { LD_DEBUG_ASSERT(mSize > 0); return mData[mSize - 1]; }
-		inline const T& Back() const  { LD_DEBUG_ASSERT(mSize > 0); return mData[mSize - 1]; }
+    inline const T* End() const
+    {
+        return mData + mSize;
+    }
 
-		inline const T* Begin() const { return mData; }
-		inline const T* End() const { return mData + mSize; }
-		inline T* Begin() { return mData; }
-		inline T* End() { return mData + mSize; }
+    inline T* Begin()
+    {
+        return mData;
+    }
 
-		// STL backwards support
-		inline const T* begin() const { return Begin(); }
-		inline const T* end() const { return End(); }
-		inline T* begin() { return Begin(); }
-		inline T* end() { return End(); }
+    inline T* End()
+    {
+        return mData + mSize;
+    }
 
-		inline View<T> GetView()
-		{
-			return View<T> { mSize, mData };
-		}
+    // STL backwards support
+    inline const T* begin() const
+    {
+        return Begin();
+    }
 
-		inline T& operator[](size_t index)
-		{
-			LD_DEBUG_ASSERT(index < mSize);
-			return mData[index];
-		}
+    inline const T* end() const
+    {
+        return End();
+    }
 
-		inline const T& operator[](size_t index) const
-		{
-			LD_DEBUG_ASSERT(index < mSize);
-			return mData[index];
-		}
+    inline T* begin()
+    {
+        return Begin();
+    }
 
-	private:
-		void Deallocate()
-		{
-			if (mData == nullptr)
-				return;
+    inline T* end()
+    {
+        return End();
+    }
 
-			for (size_t i = 0; i < mAllocSize; i++)
-				mData[i].~T();
+    inline View<T> GetView()
+    {
+        return View<T>{mSize, mData};
+    }
 
-			mAllocator.Free(mData);
-			mData = nullptr;
-			mSize = 0;
-			return;
-		}
+    inline T& operator[](size_t index)
+    {
+        LD_DEBUG_ASSERT(index < mSize);
+        return mData[index];
+    }
 
-		T* mData = nullptr;
-		size_t mSize = 0;
-		size_t mAllocSize = 0;
-		TAllocator mAllocator{};
-	};
+    inline const T& operator[](size_t index) const
+    {
+        LD_DEBUG_ASSERT(index < mSize);
+        return mData[index];
+    }
 
+protected:
+    size_t mSize = 0;
+    T* mData = nullptr;
+};
+
+template <typename T>
+class Vector : public VectorBase<T>
+{
+public:
+    Vector() = default;
+
+    Vector(size_t size)
+    {
+        Resize(size);
+    }
+
+    Vector(const Vector<T>& other)
+    {
+        Resize(other.mSize);
+        std::copy(other.Begin(), other.End(), Begin());
+    }
+
+    Vector(const std::initializer_list<T>& list)
+    {
+        Resize(list.size());
+        std::copy(list.begin(), list.end(), Begin());
+    }
+
+    ~Vector()
+    {
+        Resize(0);
+    }
+
+    Vector<T>& operator=(const Vector<T>& other)
+    {
+        Resize(other.mSize);
+        std::copy(other.Begin(), other.End(), Begin());
+        return *this;
+    }
+
+    virtual void Resize(size_t size) override
+    {
+        if (size <= mSize)
+        {
+            for (size_t i = size; i < mSize; i++)
+            {
+                mData[i].~T();
+            }
+            mSize = size;
+            return;
+        }
+
+        mBuffer.Resize(sizeof(T) * size);
+        mData = (T*)mBuffer.Data();
+
+        for (size_t i = mSize; i < size; i++)
+        {
+            new (mData + i) T();
+        }
+
+        mSize = size;
+    }
+
+private:
+    Buffer mBuffer;
+};
 
 } // namespace LD
