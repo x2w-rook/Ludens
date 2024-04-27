04296911816fcd8e8e4e71e62144730ff40b7b3a
x2w <x2w.sloth@gmail.com>
2024-04-27 16:35:11 +0800

    DSA, Stack

--- Ludens/Core/DSA/Tests/DSATests.cpp
+++ Ludens/Core/DSA/Tests/DSATests.cpp
@@ -1,7 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include "Core/DSA/Tests/DSATests.h"
 #include "Core/DSA/Tests/TestBuffer.h"
 #include "Core/DSA/Tests/TestArray.h"
+#include "Core/DSA/Tests/TestStack.h"
 #include "Core/DSA/Tests/TestVector.h"
 #include "Core/DSA/Tests/TestOptional.h"
+
+int Foo::CtorCounter = 0;
+int Foo::DtorCounter = 0;
--- Ludens/Core/DSA/Tests/TestVector.h
+++ Ludens/Core/DSA/Tests/TestVector.h
@@ -2,64 +2,9 @@
 
 #include <doctest.h>
 #include "Core/DSA/Include/Vector.h"
+#include "Core/DSA/Tests/DSATests.h"
 
 using namespace LD;
-
-class Foo
-{
-public:
-    Foo()
-    {
-        Value = 0;
-        CtorCounter++;
-    }
-
-    Foo(int value) : Value(value)
-    {
-        CtorCounter++;
-    }
-
-    Foo(const Foo& other)
-    {
-        Value = other.Value;
-    }
-
-    ~Foo()
-    {
-        DtorCounter++;
-    }
-
-    Foo& operator=(const Foo& other)
-    {
-        Value = other.Value;
-        return *this;
-    }
-
-    int Value;
-
-    static void Reset()
-    {
-        CtorCounter = 0;
-        DtorCounter = 0;
-    }
-    
-    static int CtorCount()
-    {
-        return CtorCounter;
-    }
-
-    static int DtorCount()
-    {
-        return DtorCounter;
-    }
-
-private:
-    static int CtorCounter;
-    static int DtorCounter;
-};
-
-int Foo::CtorCounter = 0;
-int Foo::DtorCounter = 0;
 
 TEST_CASE("Vector ctor")
 {
