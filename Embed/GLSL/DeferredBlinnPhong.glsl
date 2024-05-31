076c9eae096b3dabeaa1554392dfcaaf1a5c864b
x2w <x2w.rook@gmail.com>
2024-05-31 23:12:55 +0800

    GLSL Shaders
    GLSL Shaders will first be compiled into SPIRV and then embedded into the final application.
    This is automated by CMake, our Embed.py script, and the LudensBuilder.

---
+++ Embed/GLSL/DeferredBlinnPhong.glsl
@@ -0,0 +1,109 @@
+#ludens group 0 FrameStatic
+#ludens group 1 Viewport
+#ludens group 2 GBuffer
+
+#ludens vertex
+
+#version 450 core
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexUV;
+layout (location = 0) out vec2 vTexUV;
+
+void main()
+{
+	vTexUV = aTexUV;
+	gl_Position = vec4(aPos, 0.0, 1.0);
+}
+
+#ludens fragment
+
+#version 450 core
+layout (location = 0) in vec2 vTexUV;
+layout (location = 0) out vec4 fColor;
+
+struct PointLightUBO
+{
+	vec4 PosRadius;
+	vec4 Color;
+};
+
+layout (group = 0, binding = 0, std140) uniform LightingUBO
+{
+	vec4 DirLight;
+	vec4 DirLightColor;
+	PointLightUBO PointLights[128];
+} uLightingUBO;
+
+layout (group = 1, binding = 0, std140) uniform ViewportUBO
+{
+	mat4 ViewMat;
+	mat4 ProjMat;
+	mat4 ViewProjMat;
+	vec4 ViewPos;
+	vec2 Viewport;
+	int PointLightStart;
+	int PointLightCount;
+} uViewportUBO;
+
+// TODO: Deferred BGL
+layout (group = 2, binding = 0) uniform sampler2D uPosition;
+layout (group = 2, binding = 1) uniform sampler2D uNormal;
+layout (group = 2, binding = 2) uniform sampler2D uAlbedo;
+
+void main()
+{
+	vec2 uv = vTexUV;
+
+#ifdef LD_VULKAN
+	uv.y = 1.0 - uv.y;
+#endif
+
+	vec4 albedoSpec = texture(uAlbedo, uv);
+	vec3 position = texture(uPosition, uv).rgb;
+	vec3 normal = texture(uNormal, uv).rgb;
+	vec3 albedo = albedoSpec.rgb;
+	float specular = albedoSpec.a;
+	vec3 color;
+
+	int rawTexture = int(uLightingUBO.DirLight.w);
+	if (rawTexture == 1)
+		color = position;
+	else if (rawTexture == 2)
+		color = normal;
+	else if (rawTexture == 3)
+		color = albedo;
+	else
+	{
+		// single directional light
+		vec3 lightPos;
+		vec3 lightColor = uLightingUBO.DirLightColor.rgb;
+		vec3 lightDir = normalize(-uLightingUBO.DirLight.xyz);
+		vec3 viewPos = uViewportUBO.ViewPos.xyz;
+		vec3 viewDir = normalize(viewPos - position);
+		vec3 halfwayDir = normalize(lightDir + viewDir);
+		
+		// diffuse
+		color = albedo * 0.1;
+
+		color += max(dot(normal, lightDir), 0.0) * albedo * lightColor;
+		color += pow(max(dot(normal, halfwayDir), 0.0), 16.0) * specular * lightColor;
+
+		// point lights
+		for (int i = 0; i < uViewportUBO.PointLightCount; i++)
+		{
+			int idx = uViewportUBO.PointLightStart + i;
+			lightPos = uLightingUBO.PointLights[idx].PosRadius.xyz;
+			lightColor = uLightingUBO.PointLights[idx].Color.rgb;
+			lightDir = normalize(lightPos - position);
+			halfwayDir = normalize(lightDir + viewDir);
+
+			float distance = length(lightPos - position);
+			float attenuation = 1.0 / (1.0 + 0.09 * distance + 0.032 * (distance * distance)); 
+
+			color += max(dot(normal, lightDir), 0.0) * albedo * lightColor * attenuation * 0.6;
+			color += pow(max(dot(normal, halfwayDir), 0.0), 16.0) * specular * lightColor;
+		}
+	}
+
+	fColor = vec4(color, 1.0);
+}
---
+++ Embed/GLSL/GBuffer.glsl
@@ -0,0 +1,101 @@
+#ludens group 0 Viewport
+#ludens group 1 Material
+
+#ludens vertex
+
+#version 450 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexUV;
+layout (location = 3) in vec4 aModel[3];
+
+layout (location = 0) out vec3 vPos;
+layout (location = 1) out vec3 vNormal;
+layout (location = 2) out vec2 vTexUV;
+
+layout (group = 0, binding = 0, std140) uniform Viewport
+{
+	mat4 View;
+	mat4 Proj;
+	mat4 ViewProj;
+	vec4 CameraPos;
+} uViewport;
+
+void main()
+{
+	mat4 model; 
+	model[0] = vec4(aModel[0].x, aModel[1].x, aModel[2].x, 0.0);
+	model[1] = vec4(aModel[0].y, aModel[1].y, aModel[2].y, 0.0);
+	model[2] = vec4(aModel[0].z, aModel[1].z, aModel[2].z, 0.0);
+	model[3] = vec4(aModel[0].w, aModel[1].w, aModel[2].w, 1.0);
+
+	// NOTE: we are not using a normal matrix to get world normals,
+	//       the model matrix MUST use uniform scale, or the world normals
+	//       will no longer be perpendicular to the surface.
+	
+
+	vPos = (model * vec4(aPos, 1.0)).xyz;  // world pos
+	vNormal = mat3(model) * aNormal;       // world normal
+	vTexUV = aTexUV;
+
+	gl_Position = uViewport.ViewProj * vec4(vPos, 1.0);
+}
+
+#ludens fragment
+
+
+#version 450 core
+layout (location = 0) in vec3 vPos;
+layout (location = 1) in vec3 vNormal;
+layout (location = 2) in vec2 vTexUV;
+
+layout (location = 0) out vec4 fPos;
+layout (location = 1) out vec4 fNormal;
+layout (location = 2) out vec4 fAlbedoSpec;
+
+layout (group = 0, binding = 0, std140) uniform Viewport
+{
+	mat4 View;
+	mat4 Proj;
+	mat4 ViewProj;
+	vec4 CameraPos;
+} uViewport;
+
+layout (group = 1, binding = 0, std140) uniform Material
+{
+	vec3 Ambient;
+	float UseAmbientTexture;
+	vec3 Albedo;
+	float UseAlbedoTexture;
+	vec3 Specular;
+	float UseSpecularTexture;
+} uMaterial;
+
+layout (group = 1, binding = 1) uniform sampler2D uAmbient;
+layout (group = 1, binding = 2) uniform sampler2D uAlbedo;
+layout (group = 1, binding = 3) uniform sampler2D uSpecular;
+
+float avg(vec3 v)
+{
+	return (v.r + v.g + v.b) / 3.0f;
+}
+
+void main()
+{
+	float specular = avg(uMaterial.Specular);
+	vec4 albedoSpec = vec4(uMaterial.Albedo, specular);
+
+	if (uMaterial.UseAlbedoTexture > 0.0)
+	{
+		albedoSpec.rgb = texture(uAlbedo, vTexUV).rgb;
+	}
+
+	if (uMaterial.UseSpecularTexture > 0.0)
+	{
+		albedoSpec.a = avg(texture(uSpecular, vTexUV).rgb);
+	}
+
+	fPos = vec4(vPos, 1.0);
+	fNormal = vec4(normalize(vNormal), 1.0);
+	fAlbedoSpec = albedoSpec;
+}
