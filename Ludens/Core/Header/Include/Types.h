ff697080fdcd8aa0117d3b453a0d3c7e15a9f098
x2w <x2w.rook@gmail.com>
2024-07-04 21:50:31 +0800

    Byte type
    clang-format and add Byte type

--- Ludens/Core/Header/Include/Types.h
+++ Ludens/Core/Header/Include/Types.h
@@ -2,19 +2,22 @@
 
 #include <cstdint>
 
-namespace LD {
+namespace LD
+{
 
-	using u8 = unsigned char;
-	using u16 = uint16_t;
-	using u32 = uint32_t;
-	using u64 = uint64_t;
+using u8 = unsigned char;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
 
-	using i8 = signed char;
-	using i16 = int16_t;
-	using i32 = int32_t;
-	using i64 = int64_t;
+using i8 = signed char;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
-	using f32 = float;
-	using f64 = double;
+using f32 = float;
+using f64 = double;
+
+using Byte = unsigned char;
 
 } // namespace LD
